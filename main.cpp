48822ae62fcef5ee46748ae944710feda47c4132
mhosein <mhmtsze@gmail.com>
2025-12-09 15:01:53 +0330

    feat(main): add if breaks says

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
+#include <fstream>
 #include "engine/core/Engine.h"
 
 using namespace std;
 
 int main() {
-
     if (init() != 0) return 1;
 
-    if (run(renderer) != 0) return 2;
+    run();
 
     quit();
 
