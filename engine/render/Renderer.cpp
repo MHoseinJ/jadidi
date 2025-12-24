e64f7fa7a1dbae52bd81fa38e9a31a7603072ac6
mhosein <mhmtsze@gmail.com>
2025-12-24 16:52:23 +0330

    feat(render): add support of image drawing

--- engine/render/Renderer.cpp
+++ engine/render/Renderer.cpp
@@ -1,21 +1,36 @@
-#include <SDL_render.h>
 #include "Renderer.h"
+#include <SDL_render.h>
 #include <vector>
 
 #include "core/Log.h"
+#include "scene/GameObject.h"
 
-void drawObjects(SDL_Renderer* renderer, const std::vector<SDL_Texture*>& textures) {
+void drawObjects(SDL_Renderer* renderer, const std::vector<GameObject>& objects, Camera& camera) {
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    for (SDL_Texture* texture : textures) {
-        if (texture != nullptr) {
-            SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-        }
+    for (const auto& obj : objects) {
+        if (obj.sprite.texture == nullptr)
+            continue;
+
+        int w, h;
+        SDL_QueryTexture(obj.sprite.texture, nullptr, nullptr, &w, &h);
+
+        w = (int)(w * obj.transform.scale.x * camera.zoom);
+        h = (int)(h * obj.transform.scale.y * camera.zoom);
+
+        SDL_Rect dst;
+        dst.x = (int)((obj.transform.position.x - camera.transform.position.x) * camera.zoom);
+        dst.y = (int)((obj.transform.position.y - camera.transform.position.y) * camera.zoom);
+
+        dst.w = w;
+        dst.h = h;
+
+        SDL_RenderCopy(renderer, obj.sprite.texture, nullptr, &dst);
     }
 
     renderLog();
-
     SDL_RenderPresent(renderer);
 }
 
--- engine/render/Renderer.h
+++ engine/render/Renderer.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <SDL_render.h>
 #include <vector>
+#include "scene/GameObject.h"
 
-void drawObjects(SDL_Renderer* renderer, const std::vector<SDL_Texture*>& textures);
+void drawObjects(SDL_Renderer* renderer, const std::vector<GameObject>& objects, Camera& camera);
 
 void drawObjects(SDL_Renderer* renderer, std::nullptr_t);
