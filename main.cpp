400f023f44515e1af8814629c66612feed28a0ec
mhosein <mhmtsze@gmail.com>
2025-12-24 16:51:39 +0330

    small fix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <fstream>
 #include "engine/core/Engine.h"
+#include <sol/sol.hpp>
 
 using namespace std;
 
 int main() {
-    if (init() != 0) return 1;
+    if (init() != 0)
+        return 1;
 
     run();
 
