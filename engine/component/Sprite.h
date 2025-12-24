9218d03eb921407c8be94d1d8104ea9f1e2d67a5
mhosein <mhmtsze@gmail.com>
2025-12-24 16:54:13 +0330

    feat(component): add sprite and transform

---
+++ engine/component/Sprite.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <SDL_render.h>
+
+struct Sprite {
+    SDL_Texture* texture;
+};
---
+++ engine/component/Transform.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "utils/math/vector.h"
+
+struct Transform {
+    Vector2 position;
+    Vector2 scale;
+};
