a71fb06ee51b77b8e005836c6b85ce4cfc288e03
mhosein <mhmtsze@gmail.com>
2026-01-29 23:07:08 +0330

    feat(scene): add vector utility

--- engine/utils/math/vector.h
+++ engine/utils/math/vector.h
@@ -3,4 +3,15 @@
 struct Vector2 {
     float x;
     float y;
+
+    void move(const Vector2 &diff) {
+        this->x += diff.x;
+        this->y += diff.y;
+    }
+
+
+    void set(const Vector2 &pos) {
+        this->x = pos.x;
+        this->y = pos.y;
+    }
 };
