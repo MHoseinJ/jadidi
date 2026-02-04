4f8e84d971b6478cfdccfc0774155f3ee1b14ffd
mhosein <mhmtsze@gmail.com>
2026-02-04 21:55:06 +0330

    feat(scenemanager): add full gameobject management

--- engine/scene/SceneManager.h
+++ engine/scene/SceneManager.h
@@ -6,10 +6,16 @@
 using json = nlohmann::json;
 
 extern std::vector<GameObject> currentScene;
+inline uint64_t nextID = 1;
 
 namespace SceneManager {
     void loadSceneJson(const std::string& sceneName);
     std::vector<GameObject>& getCurrentScene();
     GameObject& findGameObjectWithName(const std::string& name);
     GameObject& findGameObjectWithId(int id);
+    GameObject& createObject(const std::string &name);
+    void deleteObjectByNameAndTag(const std::string &name, const std::string &tag);
+    void deleteObjectById(int id);
+    void deleteAllObjectsByTag(const std::string &tag);
+    void deleteAllObjectsByName(const std::string &name);
 }
