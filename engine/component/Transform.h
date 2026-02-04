43a4bef53c4b10a2b3c563f73ffd3ba9714eb2b2
mhosein <mhmtsze@gmail.com>
2026-02-04 21:52:49 +0330

    fix(transform): add constructor to transform

--- engine/component/Transform.h
+++ engine/component/Transform.h
@@ -4,4 +4,8 @@
 struct Transform {
     Vector2 position;
     Vector2 scale;
+
+    Transform() : position(), scale(1.0f, 1.0f) {}
+    Transform(float px, float py, float sx, float sy)
+        : position(px, py), scale(sx, sy) {}
 };
