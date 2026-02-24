274063e662502402b9bfe374f628767a3f546942
mhosein <mhmtsze@gmail.com>
2026-02-24 21:58:09 +0330

    feat(transform): add deserialize for transform

--- engine/component/Transform.h
+++ engine/component/Transform.h
@@ -1,11 +1,19 @@
 #pragma once
+#include "utils/Config.h"
 #include "utils/math/vector.h"
 
-struct Transform {
+struct Transform final : Component {
     Vector2 position;
     Vector2 scale;
 
     Transform() : position(), scale(1.0f, 1.0f) {}
     Transform(float px, float py, float sx, float sy)
         : position(px, py), scale(sx, sy) {}
+
+    void DeSerialize(const json &j) override {
+        position.x = j["position"]["x"];
+        position.y = j["position"]["y"];
+        scale.x = j["scale"]["x"];
+        scale.y = j["scale"]["y"];
+    };
 };
