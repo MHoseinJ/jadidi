1a09e87b9ab879c277bbe8b95e799def6a691638
mhosein <mhmtsze@gmail.com>
2026-02-27 01:32:28 +0330

    feat(texture): add standard texture manager

--- engine/render/TextureManager.cpp
+++ engine/render/TextureManager.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <SDL_image.h>
 #include <string>
+#include <unordered_map>
 
 #include "core/Engine.h"
 #include "core/Log.h"
@@ -24,6 +25,8 @@ int initFont() {
     cout << "font loaded" << endl;
     return 0;
 }
+
+// legacy and unsafe codes down
 
 SDL_Texture* createTextureWithText(const std::string& text, SDL_Renderer* renderer, const SDL_Color color) {
     if (!renderer || !font) return nullptr;
@@ -60,4 +63,49 @@ SDL_Texture* createImageTexture(const std::string &path) {
     }
 
     return texture;
+}
+
+// working on them
+
+TextureManager &TextureManager::instance() {
+    static TextureManager inst;
+    return inst;
+}
+
+SDL_Texture *TextureManager::get(const std::string &path) {
+    if (path.empty()) return nullptr;
+
+    auto it = textures.find(path);
+    if (it != textures.end()) {
+        it->second.refCount++;
+        return it->second.texture;
+    }
+
+    SDL_Surface* surface = IMG_Load(path.c_str());
+    if (!surface) {
+        gameLog(("IMG_Load failed: " + std::string(IMG_GetError())).c_str(), ERROR);
+        return nullptr;
+    }
+
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    if (!tex) {
+        gameLog(("CreateTexture failed: " + std::string(SDL_GetError())).c_str(), ERROR);
+        return nullptr;
+    }
+
+    textures[path] = { tex, 1 };
+    return tex;
+}
+
+void TextureManager::release(const std::string &path) {
+    auto it = textures.find(path);
+    if (it == textures.end()) return;
+
+    it->second.refCount--;
+    if (it->second.refCount <= 0) {
+        SDL_DestroyTexture(it->second.texture);
+        textures.erase(it);
+    }
 }
--- engine/render/TextureManager.h
+++ engine/render/TextureManager.h
@@ -3,6 +3,11 @@
 #include <SDL_ttf.h>
 #include <string>
 #include <iostream>
+#include <SDL_image.h>
+#include <unordered_map>
+
+#include "core/Engine.h"
+#include "core/Log.h"
 
 extern TTF_Font* font;
 
@@ -10,3 +15,20 @@ int initFont();
 
 SDL_Texture* createTextureWithText(const std::string& text, SDL_Renderer* renderer, SDL_Color color);
 SDL_Texture* createImageTexture(const std::string& path);
+
+
+class TextureManager {
+public:
+    static TextureManager& instance();
+
+    SDL_Texture* get(const std::string& path);
+
+    void release(const std::string& path);
+private:
+    struct Entry {
+        SDL_Texture* texture;
+        int refCount;
+    };
+
+    std::pmr::unordered_map<std::string, Entry> textures;
+};
