de5ddf6afe7016f7bdec31602b2e2e837f5333ce
mhosein <mhmtsze@gmail.com>
2026-03-13 18:14:11 +0330

    feat(factory): add text component to factory component registry

--- engine/component/Factory.cpp
+++ engine/component/Factory.cpp
@@ -1,32 +1,11 @@
 #include "component/Factory.h"
 
 #include "Rigidbody.h"
-//
-// #include "component/Sprite.h"
-// #include "component/Animator.h"
-// #include "component/Transform.h"
+#include "Text.h"
 
-// Factory& Factory::instance() {
-//     static Factory f;
-//     return f;
-// }
-//
 void Factory::registerComponent(std::string_view name, Creator fn) {
     creators.emplace(name, std::move(fn));
 }
-//
-// std::unique_ptr<Component> Factory::create(std::string_view name) const {
-//     auto it = creators.find(std::string(name));
-//     if (it == creators.end()) return nullptr;
-//     return it->second();
-// }
-//
-// // call once on engine init
-// void registerComponents() {
-//     auto& f = Factory::instance();
-//     f.registerComponent("sprite", [] { return std::make_unique<Sprite>(); });
-//     f.registerComponent("animator", [] { return std::make_unique<Animator>(); });
-// }
 
 void registerComponents() {
     auto& f = Factory::instance();
@@ -45,6 +24,10 @@ void registerComponents() {
 
     f.registerComponent("rigidbody", [] {
         return std::make_unique<Rigidbody>();
+    });
+
+    f.registerComponent("text", [] {
+        return std::make_unique<Text>();
     });
 }
 
