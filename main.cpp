d32ab3da977ef6c2ce6d565f112cb172ef42fe2c
mhosein <mhmtsze@gmail.com>
2026-05-03 14:15:39 +0330

    feat(main): add comments

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 
 using namespace std;
 
+// entry of the engine
 int main() {
+
     if (init() != 0)
+        // the init function return non-zero value if engine should be terminated so we send non-zero exit code
         return 1;
 
+    // ok so we are ready to start the engine
     run();
 
+    // when run() function has ended we call quit() to free up and close the engine
     quit();
 
+    // we return 0 if no one has exited already
     return 0;
 }
